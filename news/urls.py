e405e71f1c0922700aece43e4eb1445eeec10463
Anton <glukhovas@yandex.ru>
2023-05-22 13:13:26 +0700

    100+Files

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from .views import PostList, PostDetail, PostAdd, PostUpdateView, PostDeleteView
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import BaseRegisterView, upgrade_me
+from django.contrib.auth.views import LogoutView
+from allauth.account.views import SignupView, LoginView
+from .views import upgrade_me
 
 urlpatterns = [
     path('', PostList.as_view(), name='blogs'),
@@ -11,7 +12,7 @@ urlpatterns = [
     path('delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
     path('login/', LoginView.as_view(template_name='sign/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='sign/logout.html'), name='logout'),
-    path('signup/', BaseRegisterView.as_view(template_name='sign/signup.html'), name='signup'),
+    path('signup/', SignupView.as_view(template_name='sign/signup.html'), name='signup'),
     path('upgrade/', upgrade_me, name='upgrade'),
 
 ]
